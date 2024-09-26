cf2cb17378d6354e6018cd774f9d4568d604fb87
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-09-26 17:16:31 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULL = {"null": True, "blank": True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    phone = models.CharField(max_length=32, unique=True, verbose_name='Номер телефона',
+                             help_text='Укажите номер телефона')
+    invite_code = models.CharField(max_length=6, verbose_name='Инвайт-код', help_text='Генерируется при регистрации')
+    invited_by = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='referrals',
+                                   verbose_name='Кем приглашён', help_text='Пользователь, который Вас пригласил',
+                                   **NULL)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.phone
